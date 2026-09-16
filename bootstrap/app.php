84b2755538d381ab6fa74609be81093246f98c4e
Tolulope Abanikannda <abanikanndatolulope@gmail.com>
2026-09-16 11:45:38 +0100

    chore: Trust all proxies to fix production URL generation

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+        
         $middleware->alias([
             'central' => EnsureCentralDomain::class,
             'platform' => EnsurePlatformUser::class,
